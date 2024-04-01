6fa9d99df6478025af390576a3d41ee4061734c1
momenanan <141549549+momenanan@users.noreply.github.com>
2024-04-01 21:53:47 +0300

    Update connectDB.java

--- src/main/java/eventplanner/connectDB.java
+++ src/main/java/eventplanner/connectDB.java
@@ -1,43 +1,26 @@
 package eventplanner;
-
+import java.sql.SQLException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
-public class ConnectDB {
-    private static ConnectDB instance;
-    private Connection connection;
-
-    private ConnectDB() {
-        // Private constructor to prevent instantiation outside of this class
+import java.sql.Statement;
+public class connectDB {
+	protected Connection connection; 
+	
+	public void testConn() throws SQLException {
+           	
+            		String url = "jdbc:postgresql://localhost:5432/postgres";
+            		String userDB ="postgres";
+            		String passwordDB="12345";
+            	   				
+            					connection =DriverManager.getConnection(url,userDB,passwordDB);;
+            		
+            		System.out.print("connected to DB done successfully");
+        
     }
-
-    public static ConnectDB getInstance() {
-        if (instance == null) {
-            instance = new ConnectDB();
-        }
-        return instance;
-    }
-
-    public void connect() throws SQLException {
-        String url = "jdbc:postgresql://localhost:5432/postgres";
-        String user = "postgres";
-        String password = "12345";
-        try {
-            connection = DriverManager.getConnection(url, user, Password);
-            logger.log(Level.INFO,"Connected to the database successfully.")
-        } catch (SQLException e) {
-            logger.log(Level.INFO,"Error connecting to the database")
-            throw e; // Propagate the exception to the caller for handling
-        }
-    }
-
-    public Connection getConnection() {
-        return connection;
-    }
-
-   
-    
+	public Connection getConnection() 
+	{
+		return connection;
+	}	
 }
