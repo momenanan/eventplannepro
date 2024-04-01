9065ba9aeb85c88f13ee09a484fc1a10c6b9d5fc
momenanan <141549549+momenanan@users.noreply.github.com>
2024-04-01 20:31:21 +0300

    Update user.java

--- src/main/java/eventplanner/user.java
+++ src/main/java/eventplanner/user.java
@@ -2,10 +2,13 @@ package eventplanner;
 
 public abstract class user {
     String email ,PhoneNumber,UserName,password;
+    boolean isLogged;
     user(){}
     user(String email,String Password){
         this.email=email;
         this.password=Password;
     }
+   
+ 
 
 }
