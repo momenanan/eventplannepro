def3492717fbc4b1c2a34311d4326e24070fc29c
momenanan <141549549+momenanan@users.noreply.github.com>
2024-04-01 21:15:53 +0300

    Update connectDB.java

--- src/main/java/eventplanner/connectDB.java
+++ src/main/java/eventplanner/connectDB.java
@@ -3,6 +3,8 @@ package eventplanner;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class ConnectDB {
     private static ConnectDB instance;
