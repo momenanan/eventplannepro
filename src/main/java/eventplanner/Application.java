85bd0da17b79adc9c9e8fdfc57565a51799db7ff
momenanan <141549549+momenanan@users.noreply.github.com>
2024-04-01 20:27:54 +0300

    Update Application.java

--- src/main/java/eventplanner/Application.java
+++ src/main/java/eventplanner/Application.java
@@ -8,6 +8,7 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.Time;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -30,8 +31,9 @@ private boolean is_offer_av;
 private boolean is_choose_pass;
 private boolean is_search_pass;
 ////////////////////////////////////
-
-
+private String Location_venue_cla;
+private int  fees_venue_cla;
+private int SP_fk_cla;
 
 public boolean get_is_venue_av(){
 return is_venue_av;		
@@ -57,6 +59,8 @@ public boolean is_book_venue_pass(boolean av1,boolean t1,boolean cap1){
 		return av1&&t1&&cap1; 
 }
 
+
+
 public boolean Does_venue_av(int v_number){
 
 	
@@ -75,8 +79,10 @@ public boolean Does_venue_av(int v_number){
 				{					
        
 					if(is_venue_av=rs_venue.getBoolean(2)){     
-             
-					return true;
+						
+               Location_venue_cla=rs_venue.getString(5);
+			   fees_venue_cla=rs_venue.getInt(6);
+               return true;
 					}		
 					
 				}
@@ -131,19 +137,27 @@ public boolean Does_venue_time(int v_number,String d,int start_at,int end_at){
 			ResultSet rs_calender = statement_calender.executeQuery(st_calender);
 		
 			
-			
+		
 			while(rs_event.next()) {
 				
 				int x1=rs_event.getInt(8);
 				int xEventIx=rs_event.getInt(1);
 				
+				
 				if(same_venue(x1,v_number)) {
+				
+					
 					while(rs_calender.next()){
-	//					JOptionPane.showMessageDialog(null,"this is start while rs_cal");
-					    	
-									if(rs_calender.getInt(5)==xEventIx){
-								String sDateCalender=rs_calender.getString(2);	
-							
+
+						String sDateCalender=rs_calender.getString(2);	
+									
+						System.out.printf("  vN: "+v_number);
+						System.out.printf("    d: "+d+"  and in cal : "+sDateCalender);
+						System.out.printf("     start_at: "+start_at);
+						System.out.printf("       end_at: "+end_at);
+			
+						if(rs_calender.getInt(5)==xEventIx){
+			
 								if(time_realistic(start_at,end_at)&&(sDateCalender.equals(d))){
 									
 									
@@ -164,12 +178,13 @@ public boolean Does_venue_time(int v_number,String d,int start_at,int end_at){
 	                           	}
 								
 								}
+								else return true;
 								}
-								}
+								}return true;
 		}
 				}
 	  
-			return false;
+			return true;
 	
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
@@ -200,8 +215,9 @@ public boolean Does_venue_capasity(int v_number,int guist_number){
 				if(rs_venue.getInt(1) == v_number){
 		//		JOptionPane.showMessageDialog(null,rs_venue.getInt(3)+"   "+guist_number);	
 					if(rs_venue.getInt(3)>guist_number){
-						
+				
 						is_venue_cap = true;
+						
 						return true;
 					}
 					else return false;
@@ -281,14 +297,14 @@ public int number_column_after_filter(int min_price,int max_price) {
 	String passwordDB="12345";
     String st_offer = "select count(feesofoffer) from offer where feesofoffer between "+min_price+"and "+max_price;
  
-    
-	
 	Connection con_offer;
 	try {
 		con_offer = DriverManager.getConnection(url,userDB,passwordDB);
 		Statement statement_offer=con_offer.createStatement();					
 		int rs_offer = statement_offer.executeUpdate(st_offer);
-
+	
+		
+		
    return rs_offer;
 	
 	} catch (SQLException e) {
@@ -361,9 +377,10 @@ public boolean choose_offer(int o_number){
 				ResultSet rs_offer = statement_offer.executeQuery(st_offer);
 				while(rs_offer.next()){
 
-					if(rs_offer.getInt(1)==o_number){
+					if((rs_offer.getInt(1)==o_number)){
 					//	JOptionPane.showMessageDialog(null,rs_offer.getInt(1)+"---"+o_number+"---"+rs_offer.getBoolean(4));
-	            		return is_choose_pass=rs_offer.getBoolean(4);
+	            	SP_fk_cla=rs_offer.getInt(5);
+						return is_choose_pass=rs_offer.getBoolean(4);
  					
 					}
 				}
@@ -401,7 +418,7 @@ public boolean get_search_vendor(int n_vendor){
 
 					if(rs_offer.getInt(5) == n_vendor){
 					//JOptionPane.showMessageDialog(null,rs_offer.getInt(1)+"---"+o_number+"---"+rs_offer.getBoolean(4));
-	            	
+					//	SP_fk_cla =rs_offer.getInt(5);
 						return is_search_pass=rs_offer.getBoolean(4);					
 					}
 				}
@@ -413,5 +430,112 @@ public boolean get_search_vendor(int n_vendor){
 				return false;
 			} 
 }
+
+////////////////////////////////////////////////////
+
+public boolean Go_book(int vID,int cap,String d,int startHourFromUser,int endHourFromUser,String amen,int uID) 
+{
+boolean ret;
+System.out.printf("1-"+Does_venue_capasity(vID,cap)+" \n");
+System.out.printf("2-"+Does_venue_time(vID,d,startHourFromUser,endHourFromUser)+"\n");
+
+if(ret= Does_venue_capasity(vID,cap)&&Does_venue_time(vID,d,startHourFromUser,endHourFromUser)&&Does_venue_av(vID)){
+
+	   String url = "jdbc:postgresql://localhost:5432/postgres";
+	   String userDB ="postgres";
+	   String passwordDB="12345";
+	   
+	  System.out.printf("Choose one of Them as offer you want: \n");	   
+	  get_List_offer();
+	  Scanner s = new Scanner(System.in); 
+	   int o_id=s.nextInt();
+	   boolean choose_pass=choose_offer(o_id);
+	
+	   if(!choose_pass) {
+		   System.out.printf("Sorry this offer not availabe \n");
+		   return false;
+		   }
+	  
+       String st_offer = "insert into event values (Default"+",'noPer','"+d+"',"+"'"+Location_venue_cla+"',"+cap+",'"+amen+"',"+uID+","+vID+","+SP_fk_cla+")";
+       
+       //  JOptionPane.showMessageDialog(null,st_offer);
+    //amen proplem
+    			Connection con_event;
+    			try {
+					con_event = DriverManager.getConnection(url,userDB,passwordDB);	
+					Statement statement_offer = con_event.createStatement();					
+			
+				
+					setCalender(d,startHourFromUser,endHourFromUser,true);
+					int x = statement_offer.executeUpdate(st_offer);
+						
+					 
+					
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				};	 
+		  
+     return true;         	
+}
+else { 
+if(!get_is_venue_av()){
+	System.out.printf("Sorry Not available \n");
+}	
+if(!get_is_venue_cap()){
+	
+System.out.printf("Sorry Tha Capasity not enough \n");	
+}
+
+if(!get_is_venue_time()){
+	System.out.printf("Sorry The Time Overlap \n");	
+}
+}
+
+return ret;
+
+}
+//////////////////////////////////
+public boolean setCalender(String D,int s,int e,boolean is_a) 
+{
+
+	Time s2 = new Time(s);
+	Time e2 = new Time(e);
+	
+	   String url = "jdbc:postgresql://localhost:5432/postgres";
+	   String userDB ="postgres";
+	   String passwordDB="12345";
+	
+	   Connection con_calender;
+	  		
+	try {
+		
+		
+		 String sqlInsertToCalender = "insert into calender values("+"default,"+"'"+D+"',"+"'"+s2+"'"+","+"'"+e2+"'"+","+SP_fk_cla+","+is_a+")";
+	     System.out.printf(sqlInsertToCalender);
+	
+		 JOptionPane.showMessageDialog(null,sqlInsertToCalender);
+		con_calender = DriverManager.getConnection(url,userDB,passwordDB);
+		Statement statement_calender = con_calender.createStatement();					
+		 int x = statement_calender.executeUpdate(sqlInsertToCalender); 
+			
+		 return true;
+	} catch (SQLException e1) {
+		// TODO Auto-generated catch block
+		e1.printStackTrace();
+		return false;
+	}	
+	
+ 
+	
+	
+}
+
+
+
+
+
+
+
 
 }
