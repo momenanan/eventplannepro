abef724a0b5c3f6ff20c3859a6b9ff467d8c705a
momenanan <141549549+momenanan@users.noreply.github.com>
2024-04-01 20:29:59 +0300

    Update UserLoginPage.java

--- src/main/java/eventplanner/UserLoginPage.java
+++ src/main/java/eventplanner/UserLoginPage.java
@@ -2,12 +2,16 @@ package eventplanner;
 
 
 import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.swing.JOptionPane;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
 public class UserLoginPage {
-    
+    private String WhoLastLogIn;
+    private boolean UserLogInPass;
 	protected boolean Admin_is_login; // declaring the flag to chekck id the admin id login to the system or not
 	protected boolean user_is_login; 
 	protected boolean sutvise_is_login;
@@ -15,12 +19,15 @@ public class UserLoginPage {
 	protected boolean login_flag;
 	
 	protected String type_admin="admin";
-	protected String type_user="user";
+	protected String type_user="users";
 	protected String type_sutvise="survise";
 	
 	protected String user_email;
 	protected String user_password;
-	
+	private int UserIDFromDB;
+	public int getUID() {
+		return UserIDFromDB;
+	}
 	public UserLoginPage() // initilazation for this calss
 	{
 		Admin_is_login=false;
@@ -39,47 +46,74 @@ public class UserLoginPage {
 		this.user_password=user_password;
 	}
 
-	public void is_valid_credentials(String user_email, String user_password) throws SQLException {
+	public boolean is_valid_credentials(String user_email, String user_password,String type_of) throws SQLException {
 		// TODO Auto-generated method stub
 		 connectDB conDB = new connectDB();
 		 conDB.testConn();
-		 String sql = "SELECT * FROM systemusers WHERE user_email = ? AND user_password = ?";
-		try ( 
-                PreparedStatement stmt = conDB.getConnection().prepareStatement(sql)) 
-		{
-	        stmt.setString(1, user_email);
-	        stmt.setString(2, user_password);
-	        ResultSet resultSet = stmt.executeQuery();
-			if(resultSet.next())
-			{
-				String type_of_user =resultSet.getString("user_type");
-				
-				this.Admin_is_login=false;
-				this.user_is_login=false;
-				this.sutvise_is_login=false;
-				this.login_flag=false;
-				
-				if(type_of_user.equals(type_admin))
-				{
-					this.Admin_is_login=true;
-					this.login_flag=true;
-				}
-				else if(type_of_user.equals(type_user)) 
-				{
-					this.user_is_login=true;
-					this.login_flag=true;
-				}  
-				else 
-				{
-					this.sutvise_is_login=true;
-					this.login_flag=true;
-				}
-			}
+		 String sql = "SELECT * FROM "+type_of+"";
+		
+		 try {
+        //        PreparedStatement stmt = conDB.getConnection().prepareStatement(sql)) 
+		       Statement stmt = conDB.getConnection().createStatement();
+				 
+				 
+	       // stmt.setString(1, user_email);
+	      //  stmt.setString(2, user_password);
+	        ResultSet resultSet = stmt.executeQuery(sql);
+			
+	        while(resultSet.next()) {
+	        	
+	        String s1=resultSet.getString(6);
+	        String s2=resultSet.getString(7);
+		   
+	        if(s1.equals(user_email)&&s2.equals(user_password)) {
+	        	 System.out.printf("s1: "+s1+"\n");
+	 		     System.out.printf("s2: "+s2+"\n");
+	 		     
+		    	
+
+
+					this.Admin_is_login=false;
+					this.user_is_login=false;
+					this.sutvise_is_login=false;
+					this.login_flag=false;
+			
+			
+			
+					if(type_of.equals(type_admin))
+					{
+						
+						this.Admin_is_login=true;
+						this.login_flag=true;
+						WhoLastLogIn=type_admin;
+					return true;
+					}
+					else if(type_of.equals(type_user)) 
+					{
+						UserIDFromDB=resultSet.getInt(1);
+						this.user_is_login=true;
+						UserLogInPass=this.login_flag=true;
+						WhoLastLogIn=type_user;
+					return true;
+					}  
+					else 
+					{
+						this.sutvise_is_login=true;
+						this.login_flag=true;
+						WhoLastLogIn=type_sutvise;
+				return true;
+					}
+								
+	     }		
+	        
+	        }
+	  return false;
 		}
 		catch(SQLException e)
 		{
 			System.out.print(e);
 		}
+		return false;
 	}
 
 	public boolean is_user_logged() 
@@ -129,4 +163,13 @@ public class UserLoginPage {
     public String getUser_email() {
         return this.user_email;
     }
+    
+    public String getWhoLogIn(){
+    return WhoLastLogIn;	
+    	
+    }
+    public boolean getIfUserLogIn(){
+    	return UserLogInPass;
+    }
+
 }
