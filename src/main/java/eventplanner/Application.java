2b4827a7c296dce6c5867802704893895764c22a
momenanan <141549549+momenanan@users.noreply.github.com>
2024-04-02 21:08:31 +0300

    Update Application.java

--- src/main/java/eventplanner/Application.java
+++ src/main/java/eventplanner/Application.java
@@ -1,14 +1,11 @@
 package eventplanner;
 
 
-//import java.awt.HeadlessException;
 import java.sql.Connection;
-//import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-//import java.sql.Time;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Scanner;
