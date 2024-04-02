cfe73bc84c2a1579d8acdbbd7dff12c910c31923
momenanan <141549549+momenanan@users.noreply.github.com>
2024-04-02 12:07:38 +0300

    Update connectDB.java

--- src/main/java/eventplanner/connectDB.java
+++ src/main/java/eventplanner/connectDB.java
@@ -1,30 +1,32 @@
 package eventplanner;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
-
 public class ConnectDB{
-	protected Connection connection; 
-		 private static final Logger logger = Logger.getLogger(ConnectDB.class.getName());
-
 	
-	public void testConn() throws SQLException {
-           	
-            		String url = "jdbc:postgresql://localhost:5432/postgres";
-            		String userDB ="postgres";
-            		String password="12345";
-            	   				
-            					connection =DriverManager.getConnection(url,userDB,password);
-            		
- logger.log(Level.INFO,"connection sussesfully");
-    }
-	public Connection getConnection() 
+	protected static Connection connection; 
+		 private static final Logger logger = Logger.getLogger(ConnectDB.class.getName());
+	
+	
+		 
+		 
+	public static Connection getConnection() 
 	{
+		String url = "jdbc:postgresql://localhost:5432/postgres";
+		String userDB ="postgres";
+		String password="12345";
+	   				
+					try {
+						connection =DriverManager.getConnection(url,userDB,password);
+					} catch (SQLException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+		
+logger.log(Level.INFO,"connection sucssesfully");
 		return connection;
 	}	
 }
